4b3790c1d4e8a0a31961b44f924587642e35dc2b
Precious Kayili <preciouskayili@gmail.com>
2025-05-11 00:04:28 +0100

    Final updates for the day

--- src/components/Index/Speakers.tsx
+++ src/components/Index/Speakers.tsx
@@ -4,21 +4,75 @@ export default function Speakers() {
   return (
     <div className="w-full bg-[#F3FAF5] py-20">
       <Container>
-        <div className="font-medium">
-          <h3 className="text-xl text-[#55B76F] uppercase">
-            MEET OUR SPEAKERS FOR SPACERS HACK 2025
+        <h3 className="font-medium text-xl text-[#55B76F] uppercase">
+          MEET OUR SPEAKERS FOR SPACERS HACK 2025
+        </h3>
+
+        <div className="">
+          <h3 className="font-medium text-2xl uppercase mt-2">
+            Prestigious Hosts
           </h3>
-          <h3 className="text-2xl uppercase">Prestigious Hosts</h3>
+          <div className="grid grid-cols-3 gap-8 mt-8">
+            {[0, 1, 2].map((i) => (
+              <div key={`speakers-${i}`} className="bg-primary p-2">
+                <div className="h-80 bg-white w-full"></div>
+
+                <div className="text-center text-white my-5">
+                  <h3 className="font-medium text-lg mb-1">Mercy Benn-Itua</h3>
+                  <p className="">Product Designer</p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
 
-        <div className="grid grid-cols-3 gap-8 mt-8">
-          <div className="bg-primary p-2">
-            <div className="h-80 bg-white w-full"></div>
+        <div className="mt-20">
+          <h3 className="font-medium text-2xl uppercase mt-2">
+            Keynote Speakers
+          </h3>
+          <div className="grid grid-cols-3 gap-8 mt-8">
+            {[0, 1, 2].map((i) => (
+              <div key={`speakers-${i}`} className="bg-primary p-2">
+                <div className="h-80 bg-white w-full"></div>
 
-            <div className="text-center text-white my-5">
-              <h3 className="font-medium text-lg mb-1">Mercy Benn-Itua</h3>
-              <p className="">Product Designer</p>
-            </div>
+                <div className="text-center text-white my-5">
+                  <h3 className="font-medium text-lg mb-1">Mercy Benn-Itua</h3>
+                  <p className="">Product Designer</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="mt-20">
+          <h3 className="font-medium text-2xl uppercase mt-2">Panelists</h3>
+          <div className="grid grid-cols-3 gap-8 mt-8">
+            {[0, 1, 2].map((i) => (
+              <div key={`speakers-${i}`} className="bg-primary p-2">
+                <div className="h-80 bg-white w-full"></div>
+
+                <div className="text-center text-white my-5">
+                  <h3 className="font-medium text-lg mb-1">Mercy Benn-Itua</h3>
+                  <p className="">Product Designer</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="mt-20">
+          <h3 className="font-medium text-2xl uppercase mt-2">Speakers</h3>
+          <div className="grid grid-cols-3 gap-8 mt-8">
+            {[0, 1, 2].map((i) => (
+              <div key={`speakers-${i}`} className="bg-primary p-2">
+                <div className="h-80 bg-white w-full"></div>
+
+                <div className="text-center text-white my-5">
+                  <h3 className="font-medium text-lg mb-1">Mercy Benn-Itua</h3>
+                  <p className="">Product Designer</p>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </Container>
